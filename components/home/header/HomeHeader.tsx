eeaa57c04f7943c9aeb670bf8ff29b23e6f30ab9
Poxie <poxen12@hotmail.com>
2022-06-22 16:09:23 +0200

    Added the home header wave separator

--- components/home/header/HomeHeader.tsx
+++ components/home/header/HomeHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HomeWaveIcon } from '../../../icons/HomeWaveIcon';
 import styles from '../../../styles/Home.module.scss';
 import { HomeHeaderAnimation } from './HomeHeaderAnimation';
 import { HomeHeaderText } from './HomeHeaderText';
@@ -6,8 +7,11 @@ import { HomeHeaderText } from './HomeHeaderText';
 export const HomeHeader = () => {
     return(
         <div className={styles['header']}>
-            <HomeHeaderText />
-            <HomeHeaderAnimation />
+            <div className={styles['header-content']}>
+                <HomeHeaderText />
+                <HomeHeaderAnimation />
+            </div>
+            <HomeWaveIcon />
         </div>
     )
 }
