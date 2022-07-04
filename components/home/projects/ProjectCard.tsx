5f82bf9751c497ceab98c4b19ed956b69020a3c8
Poxie <poxen12@hotmail.com>
2022-07-04 21:35:04 +0200

    Added scroll to project tile on project card click

--- components/home/projects/ProjectCard.tsx
+++ components/home/projects/ProjectCard.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ExternalLinkIcon } from '../../../icons/ExternalLinkIcon';
 import styles from '../../../styles/Home.module.scss';
 import { CardType } from './ProjectsContent';
 
 const ITEM_DELAY = 100;
 const ITEM_DURATION = 1200;
-export const ProjectCard: React.FC<CardType & {index: number}> = ({ title, text, links, path, tech, date, index }) => {
+const SCROLL_FROM_TOP = 200;
+export const ProjectCard: React.FC<CardType & {index: number}> = ({ id, title, text, links, path, tech, date, index }) => {
     const [isAnimationDone, setIsAnimationDone] = useState(false);
     const [isHidden, setIsHidden] = useState(true);
     const ref = useRef<HTMLDivElement>(null);
@@ -27,6 +28,13 @@ export const ProjectCard: React.FC<CardType & {index: number}> = ({ title, text,
         window.addEventListener('scroll', scroll);
         return () => window.removeEventListener('scroll', scroll);
     }, []);
+
+    // Function to scroll to project tile
+    const goToTile = useCallback(() => {
+        const element = document.querySelector(`[data-project="${id}"]`);
+        const top = (element?.getBoundingClientRect().top || 0) + window.scrollY - SCROLL_FROM_TOP;
+        window.scrollTo({ top });
+    }, [id]);
     
     const className = [
         styles['project-card'],
@@ -34,6 +42,7 @@ export const ProjectCard: React.FC<CardType & {index: number}> = ({ title, text,
     ].join(' ');
     return(
         <div 
+            onClick={goToTile}
             style={{ transitionDelay: !isAnimationDone ? `${index * ITEM_DELAY}ms` : 'unset' }} 
             className={className} 
             ref={ref}
