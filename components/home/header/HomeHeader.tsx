7a41a59dcb5e7e840c64d65054bcc92093b6826d
Poxie <poxen12@hotmail.com>
2022-08-23 21:45:38 +0200

    Preventing header animation from showing on ssr

--- components/home/header/HomeHeader.tsx
+++ components/home/header/HomeHeader.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { HomeWaveIcon } from '../../../assets/icons/HomeWaveIcon';
 import styles from '../../../styles/Home.module.scss';
-import { HomeHeaderAnimation } from './HomeHeaderAnimation';
 import { HomeHeaderText } from './HomeHeaderText';
+
+// Making sure animation is only visible to client users
+const HomeHeaderAnimation = dynamic(
+    () => import('./HomeHeaderAnimation').then(res => res.HomeHeaderAnimation),
+    { ssr: false }
+);
 
 export const HomeHeader = () => {
     return(
