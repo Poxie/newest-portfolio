64eb10a0406fc5a3f8230c5b03181f9c6a381b7e
= <poxen12@hotmail.com>
2023-02-12 16:18:18 +0100

    Added contact form to footer

--- components/home/footer/Footer.tsx
+++ components/home/footer/Footer.tsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
 import styles from '../../../styles/Home.module.scss';
 import Button from '../../button';
+import { Input } from '../../input';
 
 export const Footer = () => {
     const [hidden, setHidden] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [feedback, setFeedback] = useState('');
     const ref = useRef<HTMLDivElement>(null);
+    const name = useRef<HTMLInputElement>(null);
+    const email = useRef<HTMLInputElement>(null);
+    const message = useRef<HTMLInputElement>(null);
 
     // Checking if content is within threshold within viewport
     useEffect(() => {
@@ -23,6 +29,35 @@ export const Footer = () => {
         return () => window.removeEventListener('scroll', scroll);
     }, []);
 
+    // Sending message
+    const sendMessage = async (e: FormEvent) => {
+        if(!name.current || !email.current || !message.current) return;
+        e.preventDefault();
+
+        // Sending message request
+        setLoading(true);
+        const data = await fetch('/api/message', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name.current.value,
+                email: email.current.value,
+                message: message.current.value
+            })
+        });
+        setLoading(false);
+
+        // Success
+        if(data.status === 200) {
+            setFeedback('Thank you for your message.')
+            name.current.value = '';
+            email.current.value = '';
+            message.current.value = '';
+            return;
+        }
+        // Error
+        setFeedback('Something went wrong while sending message.' );
+    }
+
     const className = [
         styles['footer'],
         hidden ? styles['hidden'] : ''
@@ -32,13 +67,38 @@ export const Footer = () => {
             <span>
                 Well, are you ready?
             </span>
-            
-            <Button 
-                href={process.env.NEXT_PUBLIC_MAIL_LINK}
-                className={styles['footer-button']}
+
+            <form 
+                onSubmit={sendMessage}
+                className={styles['contact-form']}
             >
-                Let&apos;s do this!
-            </Button>
+                {feedback && (
+                    <p className={styles['feedback']}>
+                        {feedback}
+                    </p>
+                )}
+                <Input 
+                    label={'Name'}
+                    ref={name}
+                />
+                <Input 
+                    label={'Email'}
+                    ref={email}
+                />
+                <Input 
+                    label={'Message'}
+                    ref={message}
+                    textArea
+                />
+                <Button 
+                    className={styles['footer-button']}
+                    type={'hollow'}
+                    disabled={loading}
+                    buttonType={'submit'}
+                >
+                    {loading ? 'Sending message...' : 'Create Connection'}
+                </Button>
+            </form>
         </footer>
     )
 }
