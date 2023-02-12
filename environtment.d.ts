7c9384a2f7ad22b2a1cf065ec6c113a28414479e
= <poxen12@hotmail.com>
2023-02-12 16:17:55 +0100

    Added api route to send message

--- environtment.d.ts
+++ environtment.d.ts
@@ -5,6 +5,11 @@ declare global {
             NEXT_PUBLIC_MAIL_LINK: string;
             NEXT_PUBLIC_WEBSITE_TITLE: string;
             NEXT_PUBLIC_WEBSITE_DESCRIPTION: string;
+            EMAIL_SENDER: string;
+            EMAIL_RECEIVER: string;
+            EMAIL_PASSWORD: string;
+            EMAIL_SUBJECT: string;
+            EMAIL_FROM: string;
         }
     }
 }
