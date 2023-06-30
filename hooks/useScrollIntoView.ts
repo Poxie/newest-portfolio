35d26a9bd4d3c083fc9beaac90b29c69fdb02060
= <poxen12@hotmail.com>
2023-06-30 17:20:13 +0200

    Added hook for element visibility

---
+++ hooks/useScrollIntoView.ts
@@ -0,0 +1,28 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useScrollIntoView = (ref: RefObject<HTMLDivElement>, options: {
+    animationDuration: number;
+}) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [isAnimationDone, setIsAnimationDone] = useState(false);
+
+    // Checking if content is within threshold within viewport
+    useEffect(() => {
+        const scroll = () => {
+            if(!ref.current) return;
+            const fromTop = ref.current.getBoundingClientRect().top;
+            const percent = fromTop / window.innerHeight;
+            
+            if(percent < .8) {
+                setIsVisible(true);
+                setTimeout(() => setIsAnimationDone(true), options.animationDuration);
+            }
+        }
+        scroll();
+
+        window.addEventListener('scroll', scroll);
+        return () => window.removeEventListener('scroll', scroll);
+    }, [options.animationDuration]);
+
+    return { isAnimationDone, isVisible };
+}
